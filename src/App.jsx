96943fe969207e788616ed54c3075254d6d55e98
Abdullahi Matazu <abdulmataz@gmail.com>
2025-04-23 16:25:18 -0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./Header"
+import Footer from "./Footer"
+import Form from "./Form";
+
+function App() {
+
+  return(
+    <>
+    <Header></Header>
+    <Form/>
+    <Footer></Footer>
+
+    </>
+  );
+ 
+}
+
+export default App
